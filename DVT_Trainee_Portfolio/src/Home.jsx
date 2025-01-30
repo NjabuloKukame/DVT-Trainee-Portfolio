0f1f7d7df249e3b0ab9555b8d5f1a530fc362ab7
NKukame <NKukame@jhb.dvt.co.za>
2025-01-30 10:11:17 +0200

    First Commit

---
+++ DVT_Trainee_Portfolio/src/Home.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+function Home(){
+    return(
+        <h1>Hello</h1>
+    )
+}
+
+export default Home;
---
+++ DVT_Trainee_Portfolio/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './styles.css'
+import Home from './Home.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+)

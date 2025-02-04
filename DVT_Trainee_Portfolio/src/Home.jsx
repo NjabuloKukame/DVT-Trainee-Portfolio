4d81dd7ce3bbbe2c971e3ec8890892c439fd738b
PhimiTm <PMakwatse@dvtsoftware>
2025-02-04 13:41:16 +0200

    Phimi's first commit

--- DVT_Trainee_Portfolio/src/Home.jsx
+++ DVT_Trainee_Portfolio/src/Home.jsx
@@ -1,10 +1,12 @@
 import './styles.css'
 import Header from './components/Header';
+import Body from './components/Body';
 
 function Home(){
     return(
         <>
             <Header/>
+            <Body/>
         </>
         
     )
---
+++ DVT_Trainee_Portfolio/src/components/Body.jsx
@@ -0,0 +1,18 @@
+import './Body.css'
+function Body(){
+    return(
+
+        <div className="container">
+            <div className="content">
+                <p id="theTitle"> DVT Development Trainees</p >
+                <h2> This webpage is a visual representation of the continuous growth of DVT's new trainee program! </h2>
+                <p> Welcome to our team portfolio! We are a group of passionate individuals passionate individuals participating in a Software Development Trainee program, united by our love for coding and dedication to innovate. This platform showcases our unique skills, creativity, and collaborative efforts as we grow into the next generation of software developers. Explore our individual portfolios and discover the talents driving our journey toward excellence in technology.</p>
+                <button className="cta-button"> Meet The Team </button>
+            </div>
+            
+        </div>
+
+    );
+}
+
+export default Body;

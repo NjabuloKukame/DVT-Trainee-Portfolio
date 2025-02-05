1848f7837bbb2954579e49b1ba8ccf64631df027
Thabane-Ngobe <tngobe@dvt.co.za>
2025-02-05 08:54:54 +0200

    Added the results bar on Search component

--- DVT_Trainee_Portfolio/src/Search.jsx
+++ DVT_Trainee_Portfolio/src/Search.jsx
@@ -1,14 +1,13 @@
 import './styles.css';
 import './Search.css';
 import Header from './components/Header';
-
+import {projects, employees} from './MockSearch.json'
+import SearchResults from './components/SearchResults.jsx'
 function Search(){
     return(
         <>
             <Header/>
-            <div>
-                <h1>Search</h1>
-            </div>
+            <SearchResults employees={employees} projects={projects} />
         </>
         
     );

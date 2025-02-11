4f862b7a24d8ef37a30b55be49de736829ea7a59
Remoneilwe Rammekwa <rrammekwa@jhb.dvt.co.za>
2025-02-11 14:57:58 +0200

    pull to edit the code.

--- DVT_Trainee_Portfolio/src/components/SearchBar.jsx
+++ DVT_Trainee_Portfolio/src/components/SearchBar.jsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
 import "./SearchBar.css";
 import SearchIcon from '@mui/icons-material/Search';
-import SearchResults from './SearchResults';
+import Header from "./Header";
+import Search from "../Search";
 import {employees} from '../MockSearch.json';
 
 function SearchBar(){
   const [search,setSearch] = useState('')
+  const [searchResults, setResults] = useState(employees);
 
   const handleInputChange = (e) => {
     const search = e.target.value;
     setSearch(search)
 
     const filteredEmployees = employees.filter((employee) => {
-    employee.name.toLowercase().includes(search.toLowerCase())
+    return employee.name.toLowerCase().includes(search.toLowerCase())
    } 
   );
+  setResults(filteredEmployees)
 
-   < SearchResults employees={
-    filteredEmployees}/>
   }
 
     return(
+      <>
+      <Header />
         <div class="input-container">
-      <SearchIcon/>
-      <input type="text"  placeholder="Search" onChange={handleInputChange}/>
-    </div>
+          <SearchIcon/>
+          <input type="text"  placeholder="Search" onChange={handleInputChange}/>
+        </div>
+      <Search searchResults={searchResults} />
+      </>
     )
 }
 export default SearchBar;

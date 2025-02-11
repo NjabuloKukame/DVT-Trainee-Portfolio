198a5ad41fdb3d9e66a3df076456e8d61f1d2d8b
Remoneilwe Rammekwa <rrammekwa@jhb.dvt.co.za>
2025-02-11 11:13:54 +0200

    saving my progress, so I can pull.

--- DVT_Trainee_Portfolio/src/components/SearchBar.jsx
+++ DVT_Trainee_Portfolio/src/components/SearchBar.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import "./SearchBar.css";
 import SearchIcon from '@mui/icons-material/Search';
+// import {employees, projects} from './MockSearch.json';
 
 function SearchBar(){
+  const [search,setSearch] = useState('')
+
+  const handleInputChange = (e) => {
+    const search = e.target.value;
+    setSearch(search)
+
+  //   const filteredEmployees = employees.filter((employee) =>
+  //   employee.firstName.toLowercase().includes(search.toLowerCase())
+  // );
+
+  // setFilteredEmployees(filtered);
+  }
+
     return(
         <div class="input-container">
-      {/* <img width="25" height="25" src="https://img.icons8.com/ios-filled/50/search--v1.png" alt="search--v1"/> */}
       <SearchIcon/>
-      <input type="text"  placeholder="Search"/>
+      <input type="text"  placeholder="Search" onChange={handleInputChange}/>
     </div>
     )
 }
 export default SearchBar;
+
+

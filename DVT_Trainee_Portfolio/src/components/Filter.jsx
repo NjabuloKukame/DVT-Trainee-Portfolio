874fe4c7b5f4bec8a3889d4560d434f8a529e3e6
amhlongo <amhlongo@dvtsoftware.com>
2025-02-11 15:45:45 +0200

    Added toggle for filter

--- DVT_Trainee_Portfolio/src/components/Filter.jsx
+++ DVT_Trainee_Portfolio/src/components/Filter.jsx
@@ -1,7 +1,28 @@
 import "./Filter.css"
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import {employees} from "../MockSearch.json"
+import { useState } from "react";
 
 function Filter(){
+    // Get all unique languages and roles
+    const languages = employees.map((employee) => employee.skills);
+    const roles = employees.map((employee) => employee.role);
+    // Remove duplicates
+    const allLanguages = [...new Set(languages.flat())];
+    const allRoles = [...new Set(roles)];
+
+    const [selectedFilter, setSelectedFilter] = useState([]);
+
+    const handleFilterClick = (filter) => {
+        if(selectedFilter.includes(filter)){
+            setSelectedFilter((prev) => prev.filter((item) => item !== filter));
+        }else{
+            setSelectedFilter((prev) => [...prev, filter]);
+        }
+
+        console.log(selectedFilter);
+        
+    }
     return(
         <section className="filter-container">
             <div className="filter-title">
@@ -13,95 +34,68 @@ function Filter(){
                 <div className="filter-section">
                     <p className="filter-section-title">Languages</p>
                     <div className="filter-content-container">
-                        <div className="filter-content">
-                            <span className="filter-content-circle"></span>
-                            <p className="filter-content-name">JavaScript</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-1"></span>
-                            <p className="filter-content-name">React</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-2"></span>
-                            <p className="filter-content-name">C#</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-3"></span>
-                            <p className="filter-content-name">CSS</p>
-                        </div>
+                        {allLanguages.map((language) => (
+                            <FilterItem 
+                                key={language}
+                                name={language} 
+                                onToggle={handleFilterClick}
+                                isSelected={selectedFilter.includes(language)}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div className="divider"></div>
                 <div className="filter-section">
                     <p className="filter-section-title">Roles</p>
                     <div className="filter-content-container">
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-4"></span>
-                            <p className="filter-content-name">Full Stack</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-5"></span>
-                            <p className="filter-content-name">Front End</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-6"></span>
-                            <p className="filter-content-name">Back End</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-7"></span>
-                            <p className="filter-content-name">Dev Ops</p>
-                        </div>
+                        {allRoles.map((role) => (
+                            <FilterItem name={role}/>
+                        ))}
                     </div>
                 </div>
                 <div className="divider"></div>
                 <div className="filter-section">
                     <p className="filter-section-title">Tools</p>
-                {/* <div className="divider"></div> */}
-                    {/* <div className="divider"></div> */}
                     <div className="filter-content-container">
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-8"></span>
-                            <p className="filter-content-name">Docker</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-9"></span>
-                            <p className="filter-content-name">Jenkins</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle color-10"></span>
-                            <p className="filter-content-name">CSS</p>
-                        </div>
+                        <FilterItem name={"React"}/>
+                        <FilterItem name={"Node.js"}/>
+                        <FilterItem name={"Express"}/>
+                        <FilterItem name={"MongoDB"}/>
+                        <FilterItem name={"MySQL"}/>
+                        <FilterItem name={"PostgreSQL"}/>   
+                        <FilterItem name={"Docker"}/>
+                        <FilterItem name={"Jenkins"}/>
                     </div>
                 </div>
                 <div className="divider"></div>
                 <div className="filter-section">
                     <p className="filter-section-title">Operating System</p>
-                {/* <div className="divider"></div> */}
-                    {/* <div className="divider"></div> */}
                     <div className="filter-content-container">
-                        <div className="filter-content">
-                            <span className="filter-content-circle"></span>
-                            <p className="filter-content-name">iOS</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle blue"></span>
-                            <p className="filter-content-name">Windows</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle red"></span>
-                            <p className="filter-content-name">Android</p>
-                        </div>
-                        <div className="filter-content">
-                            <span className="filter-content-circle yellow"></span>
-                            <p className="filter-content-name">Linux</p>
-                        </div>
+                        <FilterItem name={"Windows"}/>
+                        <FilterItem name={"Linux"}/>
+                        <FilterItem name={"MacOS"}/>
+                        <FilterItem name={"iOS"}/>
+                        <FilterItem name={"Android"}/>
                     </div>
                 </div>
             </div>
-            
-
         </section>
     );
+}
+
+
+
+function FilterItem({name, onToggle, isSelected}){
+    const generatePastelColor = () => {
+        const hue = Math.floor(Math.random() * 360);
+        return `hsl(${hue}, 100%, 50%)`;
+    };
+    return(
+        <div className={`filter-content ${isSelected ? "selected" : ""}`}>
+            <span className="filter-content-circle" style={{backgroundColor: generatePastelColor()}}></span>
+            <p className="filter-content-name" onClick={() => onToggle(name)}>{name}</p>
+        </div>
+    )
 }
 
 export default Filter;

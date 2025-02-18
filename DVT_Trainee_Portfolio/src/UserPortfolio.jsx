9a168711b66adb7b7a37c74c56f5436fc45c9ffc
pthobei <pthobei@dvtsoftware.com>
2025-02-18 15:30:34 +0200

    Refactor button layout and styles in UserPortfolio component

--- DVT_Trainee_Portfolio/src/UserPortfolio.jsx
+++ DVT_Trainee_Portfolio/src/UserPortfolio.jsx
@@ -6,31 +6,31 @@ import LinkedIn from './assets/icons8-linkedin-100.png'
 import Contact from './assets/icons8-contact-100.png'
 import Header from './components/Header';
 
-function UserPortfolio(){
-    const projects = [0,0,0,0];
-    return(
+function UserPortfolio() {
+    const projects = [0, 0, 0, 0];
+    return (
         <>
-            <Header/>
+            <Header />
             <div className="portfolio-container">
 
                 <div className="profile-header">
                     <div className="pop">
-                    <img src={ProfileImage} alt="Profile" className="profile-img" />
-                    <div className="user-name-section">
-                        <h1>John Doe</h1>
-                    </div>
+                        <img src={ProfileImage} alt="Profile" className="profile-img" />
+                        <div className="user-name-section">
+                            <h1>John Doe</h1>
+                        </div>
 
-                    <div className='user-position'>
-                        <h3>Full Stack Developer</h3>
-                    </div>
+                        <div className='user-position'>
+                            <h3>Full Stack Developer</h3>
+                        </div>
                     </div>
                     <div className="about-section">
                         <p>I am a fullstack developer at DVT... </p>
-                </div>
+                    </div>
                 </div>
 
 
-                
+
 
                 <div className="technology-section">
                     <div className="technology-stack-section">
@@ -50,32 +50,35 @@ function UserPortfolio(){
 
                         <h2>Projects</h2>
                         <div className="project-grid-section">
-                            {projects.map( (project) => {
+                            {projects.map((project) => {
                                 return (<div className='project'></div>)
                             })}
                         </div>
 
                     </div>
+
+                </div>
+                <div className="button-container">
+                    <div className="cv-button">
+                        <button className="cv-btn">Download CV</button>
+                    </div>
+
+                    <div className="button-section">
+                        <button className="linkedin-btn">
+                            <img src={LinkedIn} alt="LinkedIn" className="linkedin-img" />
+                        </button>
+                        <button className="github-btn">
+                            <img src={Github} alt="Github" className="github-img" />
+                        </button>
+                        <button className="contact-btn">
+                            <abbr title='Contact'><img src={Contact} alt="Contact" className="contact-img" /></abbr>
+                        </button>
+
+                    </div>
+                    
                     
                 </div>
-                    <div>
-                        <div className='cv-button'>
-                            <button className="cv-btn">Download CV</button>
-                        </div>
-
-                        <div className="button-section">
-                            
-                            <button className="linkedin-btn">
-                                <img src={LinkedIn} alt="LinkedIn" className="linkedin-img" />
-                            </button>
-                            <button className="github-btn">
-                                <img src={Github} alt="Github" className="github-img" />
-                            </button>
-                            <button className="contact-btn">
-                                <img src={Contact} alt="Contact" className="contact-img" />
-                            </button>
-                        </div>
-                </div>
+                <button className='update-btn'>Update Profile</button>
             </div>
         </>
     );

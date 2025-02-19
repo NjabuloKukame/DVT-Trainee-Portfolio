bf3a2a8b195ee620fa86f7f889d8dd86559994cb
PhimiTm <PMakwatse@dvtsoftware>
2025-02-19 09:57:24 +0200

    Profile Card Deletion & JSON File Creation

--- DVT_Trainee_Portfolio/src/Portfolio.jsx
+++ DVT_Trainee_Portfolio/src/Portfolio.jsx
@@ -19,38 +19,7 @@ function Portfolio() {
             <section className="cards">
                 <div className="carousel">
                     <button className="carousel-navigation prev">&#10094;</button>
-                    <div className="carousel-inner-full">
-                    <Link to="/UserPortfolio" >
-                        <div className="carousel-item-top">
-                            <img src="images/1.jpg" alt="Image 1" />
-                            <div className="carousel-caption">
-                                <p>Name <br/>
-                                    <span>Surname
-                                    Personality trait
-                                    </span>
-                                
-                                </p>
-                                {/* <p>Surname</p>
-                                <p>Personality trait</p> */}
-                            </div>
-                        </div>
-                        <p id="project-Title">My Projects</p>
-                        <div className="carousel-item-middle">
-                            <div>&nbsp;</div>
-                            <div>&nbsp;</div>
-                            <div>&nbsp;</div>
-                            <div>&nbsp;</div>
-                            <div>&nbsp;</div>
-                            <div>&nbsp;</div>
-                        </div>
-                        <div className="carousel-item-bottom">
-                            <a href="#"><i className="bi bi-github"></i></a>
-                            <a href="#"><i className="bi bi-linkedin"></i></a>
-                            <a href="#"><i className="bi bi-envelope-fill"></i></a>
-                            <a href="#"><i className="bi bi-telephone"></i></a>
-                        </div>
-                        </Link>
-                    </div>
+                    
                     <button className="carousel-navigation next">&#10095;</button>
                 </div>
             </section>
